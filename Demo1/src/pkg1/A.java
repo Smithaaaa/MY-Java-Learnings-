11797dde106ce4185c537da52233a246f301593b
Smitha <53327494+Smithaaaa@users.noreply.github.com>
2026-03-04 23:01:18 +0530

    Add files via upload
    My  practice files uploading here

---
+++ Demo1/src/pkg1/A.java
@@ -0,0 +1,11 @@
+public class A {
+    public static void main(String args[]){
+
+            int n =1;
+            for(int i =10 ; i>=n; i--){
+                System.out.println(i);
+            }
+        }
+
+    }
+
