8b0648f1219fe3179af14e6a9e086d8de422e25e
Smitha <53327494+Smithaaaa@users.noreply.github.com>
2026-03-04 23:31:51 +0530

    Add files via upload

---
+++ Demo1/src/num.java
@@ -0,0 +1,14 @@
+package mango;
+
+public class num {
+	public static void main(String[] args) {
+		int sum =0;
+
+		for(int j=1;j<=10;j++) {
+			sum = sum +j;
+		}
+		System.out.println("sum of 10 natural numbers" +sum);
+				
+	}
+
+}
